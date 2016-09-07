baa4c22afe281e980b54810780c442ef6f7a1b88
root <root@NC-PH-1075-21.web-hosting.com>
2016-09-06 23:59:02 -0700

    commit on 060916

--- admin/bin/commit.sh
+++ admin/bin/commit.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-GITDIR=~/git/
+GITDIR=~/git/sitemind/
 DATE=`date +%d%m%y`
-mkdir ~/backup/commit_"$DATE"/
 BACKUP=~/backup/commit_"$DATE"/
 VERSION=$(sm-commit-version)
+
+CHECK0=$(pwd)
+CHECK1=$(ls ~/backup/commit_"$DATE")
+CHECK2=$(git tag | grep fatal | tr -d ' ')
+HOME=~/git/sitemind
+
+	if [ -z $CHECK1]; then
+		mkdir ~/backup/commit_"$DATE"/
+	fi
+
+	if [ -n $CHECK2]; then
+		git init
+	fi
 
 	rsync -av /var/www/html/dev $BACKUP
 	rsync -av /var/www/html/admin $BACKUP
@@ -13,9 +25,13 @@ VERSION=$(sm-commit-version)
 	rsync -av /var/www/html/sitemind/hello.sh $BACKUP/sitemind
 	rsync -av /var/www/html/README.md $BACKUP
 	
-	rsync -av $BACKUP $GITDIR
+	rsync -av --exclude=.git $BACKUP $GITDIR --delete
 
-	git tag -a "$VERSION" -m "version "$VERSION""
+	cd $HOME
+
+	git tag -a v"$VERSION" -m "version "$VERSION""
 	git add *
 	git commit -m "$VERSION commit on "$DATE""
+
+	eval `ssh-agent -s` && ssh-add ~/.ssh/git;
 	git push origin master
