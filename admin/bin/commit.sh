61ac2545907baa9cfe52a14d532b8646bee29eff
root <root@NC-PH-1075-21.web-hosting.com>
2016-09-08 04:21:45 -0700

    v0.9.7 commit on 080916

--- admin/bin/commit.sh
+++ admin/bin/commit.sh
@@ -3,7 +3,7 @@
 GITDIR=~/git/sitemind/
 DATE=`date +%d%m%y`
 BACKUP=~/backup/commit_"$DATE"/
-VERSION=`sm-commit-version`
+VERSION=$(/var/www/html/admin/bin/commit-version.sh)
 
 CHECK0=$(pwd)
 CHECK1=$(ls ~/backup/commit_"$DATE")
